3ef608168e6faa192888bfdad644ab26afc56085
TLZH0001 <ztl1998@berkeley.edu>
2023-12-21 14:43:14 -0800

    adding a working model

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -71,26 +71,30 @@ class RoarCompetitionSolution:
 
     
     async def initialize(self) -> None:
-        # TODO: You can do some initial computation here if you want to.
-        # For example, you can compute the path to the first waypoint.
+        self.model = SAC.load("./sample_model", env=None)
 
+
+        ###### initialize RL environment #############
         nest_asyncio.apply()
         ''' get env '''
         self.env = roar_py_rl_carla.RoarRLCarlaSimEnv(
-        self.vehicle,
-        self.maneuverable_waypoints,
-        self.location_sensor,
-        self.rpy_sensor,
-        self.local_velocimeter_sensor,
-        self.collision_sensor,
-        waypoint_information_distances=set([-10.0, 0.0, 1.0, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0, 100.0]),
-        world = self.world,
-        collision_threshold = 10.0
+            self.vehicle,
+            self.maneuverable_waypoints,
+            self.location_sensor,
+            self.rpy_sensor,
+            self.local_velocimeter_sensor,
+            self.collision_sensor,
+            waypoint_information_distances=set([-10.0, 0.0, 1.0, 2.0, 3.0, 5.0, 7.0, 10.0, 15.0, 20.0, 30.0, 40.0, 50.0, 100.0]),
+            world = self.world,
+            collision_threshold = 10.0
         )
         self.env = SimplifyCarlaActionFilter(self.env)
         self.env = gym.wrappers.FilterObservation(self.env, ["gyroscope", "waypoints_information", "local_velocimeter"])
         self.env = gym.wrappers.FlattenObservation(self.env)
         self.env = roar_py_rl_carla.FlattenActionWrapper(self.env)
+        self.env = gym.wrappers.RecordVideo(env, f"videos/{wandb_run.name}_eval_{wandb_run.id}")
+        self.env = Monitor(env, f"logs/{wandb_run.name}_{wandb_run.id}", allow_early_resets=True)
+
 
         ''' get RL model '''
         model_path = "models/rl_model_4099796_steps"
@@ -103,17 +107,17 @@ class RoarCompetitionSolution:
         self.obs, info = self.env.reset()
         # print('OBS:', self.obs)
 
-        # Receive location, rotation and velocity data 
-        vehicle_location = self.location_sensor.get_last_gym_observation()
-        vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
-        vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
+        # # Receive location, rotation and velocity data 
+        # vehicle_location = self.location_sensor.get_last_gym_observation()
+        # vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
+        # vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
 
-        self.current_waypoint_idx = 10
-        self.current_waypoint_idx = filter_waypoints(
-            vehicle_location,
-            self.current_waypoint_idx,
-            self.maneuverable_waypoints
-        )
+        # self.current_waypoint_idx = 10
+        # self.current_waypoint_idx = filter_waypoints(
+        #     vehicle_location,
+        #     self.current_waypoint_idx,
+        #     self.maneuverable_waypoints
+        # )
 
 
     async def step(
@@ -130,45 +134,47 @@ class RoarCompetitionSolution:
         action, _state = self.model.predict(self.obs, deterministic=False)
         # print('ACTION:', action)
         self.obs, reward, terminated, truncated, info = self.env.step(action) # this statement causes error
+        return action
 
-        # Receive location, rotation and velocity data 
-        vehicle_location = self.location_sensor.get_last_gym_observation()
-        vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
-        vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
-        vehicle_velocity_norm = np.linalg.norm(vehicle_velocity)
+
+        # # Receive location, rotation and velocity data 
+        # vehicle_location = self.location_sensor.get_last_gym_observation()
+        # vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
+        # vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
+        # vehicle_velocity_norm = np.linalg.norm(vehicle_velocity)
         
-        # Find the waypoint closest to the vehicle
-        self.current_waypoint_idx = filter_waypoints(
-            vehicle_location,
-            self.current_waypoint_idx,
-            self.maneuverable_waypoints
-        )
-         # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
-        waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + 3) % len(self.maneuverable_waypoints)]
+        # # Find the waypoint closest to the vehicle
+        # self.current_waypoint_idx = filter_waypoints(
+        #     vehicle_location,
+        #     self.current_waypoint_idx,
+        #     self.maneuverable_waypoints
+        # )
+        #  # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
+        # waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + 3) % len(self.maneuverable_waypoints)]
 
-        # Calculate delta vector towards the target waypoint
-        vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
-        heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
+        # # Calculate delta vector towards the target waypoint
+        # vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
+        # heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
 
-        # Calculate delta angle towards the target waypoint
-        delta_heading = normalize_rad(heading_to_waypoint - vehicle_rotation[2])
+        # # Calculate delta angle towards the target waypoint
+        # delta_heading = normalize_rad(heading_to_waypoint - vehicle_rotation[2])
 
-        # Proportional controller to steer the vehicle towards the target waypoint
-        steer_control = (
-            -8.0 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
-        ) if vehicle_velocity_norm > 1e-2 else -np.sign(delta_heading)
-        steer_control = np.clip(steer_control, -1.0, 1.0)
+        # # Proportional controller to steer the vehicle towards the target waypoint
+        # steer_control = (
+        #     -8.0 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
+        # ) if vehicle_velocity_norm > 1e-2 else -np.sign(delta_heading)
+        # steer_control = np.clip(steer_control, -1.0, 1.0)
 
-        # Proportional controller to control the vehicle's speed towards 40 m/s
-        throttle_control = 0.05 * (20 - vehicle_velocity_norm)
+        # # Proportional controller to control the vehicle's speed towards 40 m/s
+        # throttle_control = 0.05 * (20 - vehicle_velocity_norm)
 
-        control = {
-            "throttle": np.clip(throttle_control, 0.0, 1.0),
-            "steer": steer_control,
-            "brake": np.clip(-throttle_control, 0.0, 1.0),
-            "hand_brake": 0.0,
-            "reverse": 0,
-            "target_gear": 0
-        }
-        await self.vehicle.apply_action(control)
-        return control
+        # control = {
+        #     "throttle": np.clip(throttle_control, 0.0, 1.0),
+        #     "steer": steer_control,
+        #     "brake": np.clip(-throttle_control, 0.0, 1.0),
+        #     "hand_brake": 0.0,
+        #     "reverse": 0,
+        #     "target_gear": 0
+        # }
+        # await self.vehicle.apply_action(control)
+        # return control
